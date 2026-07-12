2db4b30a438568d3b8e007fe4d0f3034eb1dd8f6
Felipi Matheuz <felipi568@gmail.com>
2026-07-12 13:28:45 -0300

    fix manifest rule

--- src/main/kotlin/consistency/rules/ManifestRule.kt
+++ src/main/kotlin/consistency/rules/ManifestRule.kt
@@ -2,6 +2,7 @@ package consistency.rules
 
 import consistency.model.ConsistencyContext
 import consistency.model.ValidationError
+import model.domain.FileSource
 
 class ManifestRule : ConsistencyRule {
 
@@ -9,11 +10,9 @@ class ManifestRule : ConsistencyRule {
         context: ConsistencyContext
     ): List<ValidationError> {
 
-        val expected = setOf(
-            "relics.json",
-            "prime_sets.json",
-            "prime_collection.json"
-        )
+        val expected = FileSource.entries.map { it.path }
+            .filter { it != FileSource.MANIFEST.path }
+            .toSet()
 
         val files = context.manifest.files
             .map { file -> file.name }
